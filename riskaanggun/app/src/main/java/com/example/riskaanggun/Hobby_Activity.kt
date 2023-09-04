e84b8c663c451d4da0248e56295a12cbb582262a
vivi012 <vivinuraisyah012@gmail.com>
2023-09-04 10:16:38 +0700

    Initial commit

---
+++ riskaanggun/app/src/main/java/com/example/riskaanggun/Hobby_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.riskaanggun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Hobby_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hobby)
+    }
+}
